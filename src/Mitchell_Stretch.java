821e16e56e9c14752601c2995b6cc7d285ef7075
Tommy Mitchell <tommym2203@gmail.com>
2022-06-02 14:26:03 -0400

    A work in progress

--- src/Mitchell_Stretch.java
+++ src/Mitchell_Stretch.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mitchell_Stretch {
@@ -6,7 +7,38 @@ public class Mitchell_Stretch {
         for (int i = 5; i > 0; i--) {
             System.out.println("Please input a valid sequence. This code will run " + i + " more time(s).");
             String[] line = reader.nextLine().split(" ");
+            String[][] grid = new String[Integer.parseInt(line[0])][Integer.parseInt(line[1])];
+
+            //Add all blocked spaces to an arrayList
+            ArrayList<Integer> blockedSpaces = new ArrayList<Integer>();
+            for (int a = 4; a < line.length; a++) {
+                blockedSpaces.add(Integer.parseInt(line[a]));
+            }
+            int startPiece = Integer.parseInt(line[2]);
+            //Use the fillGrid method to add numbers and blocked Spaces and the starting piece.
+            fillGrid(grid, blockedSpaces, startPiece);
         }
         reader.close();
+    }
+    public static String[][] fillGrid (String[][] arr, ArrayList<Integer> blocked, int start){
+        int number = 1;
+        for (int r = 0; r < arr.length; r++){
+            for (int c = 0; c < arr[r].length; c++) {
+                arr[r][c] = Integer.toString(number);
+                number++;
+            }
+        }
+        for (int rb = 0; rb < arr.length; rb++){
+            for (int cb = 0; cb < arr[rb].length; cb++) {
+               if (blocked.contains(Integer.parseInt(arr[rb][cb]))) {
+                   arr[rb][cb] = "X";
+               }
+               //This breaks TODO find a way to add a start postion without parsing a string
+               if (Integer.parseInt(arr[rb][cb]) == start) {
+                   arr[rb][cb] = "S";
+               }
+            }
+        }
+        return arr;
     }
 }
