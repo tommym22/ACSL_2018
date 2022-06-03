bd73bd80ccda701a7187d2acdcfdae3e307405ec
Tommy Mitchell <tommym2203@gmail.com>
2022-06-03 13:22:32 -0400

    WIP

--- src/Mitchell_Stretch.java
+++ src/Mitchell_Stretch.java
@@ -16,10 +16,17 @@ public class Mitchell_Stretch {
             }
             int startPiece = Integer.parseInt(line[2]);
             //Use the fillGrid method to add numbers and blocked Spaces and the starting piece.
-            fillGrid(grid, blockedSpaces, startPiece);
+            grid = fillGrid(grid, blockedSpaces, startPiece);
+            //use the placePieces method to place the pieces and print out that result
+            System.out.println(placePieces(grid));
         }
         reader.close();
     }
+    /*Take in an empty 2d array, a filled ArrayList with the blocked elements, and a starting integer
+      First, loop through and add a bunch of numbers to the grid in numerical order
+      Next, loop through and add all the blocked spaces and the starting space
+      Return this new 2d array
+     */
     public static String[][] fillGrid (String[][] arr, ArrayList<Integer> blocked, int start){
         int number = 1;
         for (int r = 0; r < arr.length; r++){
@@ -30,15 +37,40 @@ public class Mitchell_Stretch {
         }
         for (int rb = 0; rb < arr.length; rb++){
             for (int cb = 0; cb < arr[rb].length; cb++) {
-               if (blocked.contains(Integer.parseInt(arr[rb][cb]))) {
+               if (!arr[rb][cb].equals("X") && blocked.contains(Integer.parseInt(arr[rb][cb]))) {
                    arr[rb][cb] = "X";
                }
-               //This breaks TODO find a way to add a start postion without parsing a string
-               if (Integer.parseInt(arr[rb][cb]) == start) {
+
+               if ((!arr[rb][cb].equals("X") && !arr[rb][cb].equals("S")) && Integer.parseInt(arr[rb][cb]) == start) {
                    arr[rb][cb] = "S";
                }
             }
         }
         return arr;
     }
+
+    public static String placePieces(String[][]arr) {
+        String output = "";
+        char currentPiece = 'A';
+        if (currentPiece. > 3){
+
+        }
+        for (int r = 0; r < arr.length; r++){
+            for (int c = 0; c < arr[r].length; c++) {
+             if (arr[r][c].equals("S")) {
+                 if (currentPiece.equals("A")&& c+2<arr[r].length && !arr[c+2].equals("X")) {
+                     output+= "A";
+                     currentPiece = "B";
+                 }
+                 else if ((currentPiece.equals("A")&& c+2>arr[r].length)){
+                     currentPiece = "B";
+                 }
+             }
+             if ()
+
+            }
+        }
+        return output;
+    }
+
 }
